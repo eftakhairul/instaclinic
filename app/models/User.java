0d5bcffae407a28509e7d8f7460602e414a316db
Eftakhairul Islam <eftakhairul@gmail.com>
2014-02-19 17:35:09 -0500

    user class is done properly

--- app/models/User.java
+++ app/models/User.java
@@ -1,7 +1,11 @@
 package models;
 
+import org.joda.time.DateTime;
+import play.data.format.Formats;
+import play.data.validation.Constraints;
 import play.db.ebean.Model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
@@ -9,29 +13,62 @@ import javax.persistence.Id;
 public class User extends Model{
 
     @Id
-    public string id;
+    public Long id;
 
-    public string username;
+    @Column(unique=true)
+    @Constraints.Required
+    public String username;
 
-    public string password;
+    @Constraints.Required
+    public String password;
 
-    public string getId() {
+    @Constraints.Required
+    public DateTime create_date;
+
+    public static Finder<Long, User> find = new Finder(Long.class, User.class);
+
+    //Static initialization
+    User() {
+        create_date = new DateTime();
+    }
+
+    public Long getId() {
         return id;
     }
 
-    public string getUsername() {
+    public String getUsername() {
         return username;
     }
 
-    public void setUsername(string username) {
+    public void setUsername(String username) {
         this.username = username;
     }
 
-    public string getPassword() {
+    public String getPassword() {
         return password;
     }
 
-    public void setPassword(string password) {
+    public void setPassword(String password) {
         this.password = password;
+    }
+
+    public static User verify(String username, String password) {
+
+        return find.where()
+                   .eq("username", username)
+                   .eq("password", password)
+                   .findUnique();
+    }
+
+    public static void create(User newuser) {
+        newuser.save();
+    }
+
+    public static void delete(Long id) {
+        find.ref(id).delete();
+    }
+
+    public static User findById(Long id) {
+        return find.byId(id);
     }
 }
