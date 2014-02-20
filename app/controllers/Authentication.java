206934a799ff093a6bb6c9b46f814ec8ad563e78
Eftakhairul Islam <eftakhairul@gmail.com>
2014-02-19 19:30:04 -0500

    authentication is almost done

--- app/controllers/Authentication.java
+++ app/controllers/Authentication.java
@@ -1,11 +1,53 @@
 package controllers;
 
-/**
- * Created with IntelliJ IDEA.
- * User: eftakhairul
- * Date: 2/19/14
- * Time: 4:59 PM
- * To change this template use File | Settings | File Templates.
- */
-public class Authentication {
+import play.*;
+import play.data.Form;
+import play.mvc.*;
+
+import views.html.*;
+import java.util.List;
+
+import models.User;
+
+public class Authentication extends Controller {
+
+    public static User currentUser  = null;
+    static Form<User> loginForm     = form(User.class);
+
+
+    /*
+    * Serve the Login Form
+    * route: GET /login
+    */
+    public static Result login() {
+        return ok(views.html.login.render(loginForm));
+    }
+
+    /*
+    * Processing Login user
+    *
+    * route: Post /login
+    */
+    public static Result loginFormProcess() {
+
+        Form<User> filledForm = loginForm.bindFromRequest();
+
+        if(filledForm.hasErrors()) {
+            return badRequest("Something is went wrong");
+        } else {
+            User c= filledForm.get();
+            currentUser = User.verify(c.username, c.password);
+            if (currentUser != null) {
+                session("connected", currentUser.id.toString());
+                return redirect(routes.Application.index());
+            }
+
+            return ok("bad man nanana");
+        }
+    }
+
+    public static Result logout() {
+        return ok(index.render("Your new application is ready."));
+
+    }
 }
