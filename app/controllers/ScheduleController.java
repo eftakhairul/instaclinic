f6637fd0876b8b420b225605f00bd51a7cb1552b
Md. Ali Ahsan Rana <rana_cse_ruet@yahoo.com>
2014-02-22 19:20:53 -0500

    Added basic crud example with schedule.

---
+++ app/controllers/ScheduleController.java
@@ -0,0 +1,115 @@
+package controllers;
+
+import java.util.Date;
+
+import javax.media.jai.RenderableCollectionImageFactory;
+
+import com.avaje.ebean.Ebean;
+
+import play.*;
+import play.data.Form;
+import play.mvc.*;
+import models.Computer;
+import models.Schedule;
+import models.User;
+import views.html.*;
+
+public class ScheduleController extends Controller 
+{
+	/**
+	 * This result directly redirect to application home.
+	 */
+	public static Result GO_HOME = redirect(
+	    routes.ScheduleController.list(0, "name", "asc", "")
+	);
+    
+  public static Result index() 
+  {
+	Schedule temp = Ebean.find(Schedule.class, 1);
+	if(temp == null) {
+		temp = new Schedule(new Date(1000), new Date());
+	}
+	return ok();
+  }
+  
+  /**
+   * Display the paginated list of computers.
+   *
+   * @param page Current page number (starts from 0)
+   * @param sortBy Column to be sorted
+   * @param order Sort order (either asc or desc)
+   * @param filter Filter applied on computer names
+   */
+  public static Result list(int page, String sortBy, String order, String filter) {
+      return ok(
+          views.html.schedule.list.render(
+              Schedule.page(page, 10, sortBy, order, filter),
+              sortBy, order, filter
+          )
+      );
+  }
+  
+  /**
+   * Display the 'edit form' of a existing Computer.
+   *
+   * @param id Id of the computer to edit
+   */
+  public static Result edit(Long id) {
+      Form<Schedule> computerForm = form(Schedule.class).fill(
+    		  Schedule.find.byId(id)
+      );
+      return ok(
+    		  views.html.schedule.editForm.render(id, computerForm)
+      );
+  }
+  
+  /**
+   * Handle the 'edit form' submission 
+   *
+   * @param id Id of the computer to edit
+   */
+  public static Result update(Long id) {
+      Form<Schedule> computerForm = form(Schedule.class).bindFromRequest();
+      if(computerForm.hasErrors()) {
+    	  //computerForm.errors()[0].
+          return badRequest(views.html.schedule.editForm.render(id, computerForm));
+      }
+      computerForm.get().update(id);
+      flash("success", "Computer " + computerForm.get().getId() + " has been updated");
+      return GO_HOME;
+  }
+  
+  /**
+   * Display the 'new computer form'.
+   */
+  public static Result create() {
+      Form<Schedule> scheduleForm = form(Schedule.class);
+      return ok(
+    		  views.html.schedule.createForm.render(scheduleForm)
+      );
+  }
+  
+  /**
+   * Handle the 'new computer form' submission 
+   */
+  public static Result save() {
+      Form<Schedule> scheduleForm = form(Schedule.class).bindFromRequest();
+      if(scheduleForm.hasErrors()) {
+    	  //flash("error", scheduleForm.errorsAsJson());
+          return badRequest(views.html.schedule.createForm.render(scheduleForm));
+      }
+      scheduleForm.get().save();
+      flash("success", "Computer " + scheduleForm.get().getId() + " has been created");
+      return GO_HOME;
+  }
+  
+  /**
+   * Handle computer deletion
+   */
+  public static Result delete(Long id) {
+	  Schedule.find.ref(id).delete();
+      flash("success", "Computer has been deleted");
+      return GO_HOME;
+  }
+  
+}
--- app/models/Schedule.java
+++ app/models/Schedule.java
@@ -6,8 +6,13 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.OneToOne;
 
+import com.avaje.ebean.Page;
+
+import play.data.format.Formats;
 import play.data.format.Formats.DateTime;
+import play.data.validation.Constraints;
 import play.db.ebean.Model;
+import play.db.ebean.Model.Finder;
 
 @Entity
 public class Schedule extends Model 
@@ -15,16 +20,23 @@ public class Schedule extends Model
 	@Id
 	private int id;
 	
-	@DateTime(pattern = "MM/dd/yy")
+	@Formats.DateTime(pattern="yyyy-MM-dd")
+	@Constraints.Required
 	private Date startTime;
 	
-	@DateTime(pattern = "MM/dd/yy")
+	@Formats.DateTime(pattern="yyyy-MM-dd")
+	@Constraints.Required
 	private Date endTime;
 	
 	public Schedule(Date startTime, Date endTime)
 	{
 		this.startTime = startTime;
 		this.endTime   = endTime;
+	}
+	
+	public int getId()
+	{
+		return id;
 	}
 	
 	public Date getStartTime()
@@ -46,4 +58,25 @@ public class Schedule extends Model
 	{
 		this.endTime =  endTime;
 	}
+	
+	/**
+     * Generic query helper for entity Schedule with id Long
+     */
+    public static Finder<Long,Schedule> find = new Finder<Long,Schedule>(Long.class, Schedule.class); 
+    
+    /**
+     * Return a page of Schedule
+     *
+     * @param page Page to display
+     * @param pageSize Number of computers per page
+     * @param sortBy Computer property used for sorting
+     * @param order Sort order (either or asc or desc)
+     * @param filter Filter applied on the name column
+     */
+    public static Page<Schedule> page(int page, int pageSize, String sortBy, String order, String filter) {
+        return 
+            find.where()
+                .findPagingList(pageSize)
+                .getPage(page);
+    }
 }
