b08cde7bfc1513c908e4bcda43a44ceb7ff02c6b
Eftakhairul Islam <eftakhairul@gmail.com>
2014-03-02 13:28:55 -0500

    checkout is fully done

--- app/controllers/PaymentController.java
+++ app/controllers/PaymentController.java
@@ -4,14 +4,23 @@ import models.Appointment;
 import models.Payment;
 import play.data.Form;
 import play.mvc.*;
+import views.html.*;
 
 import java.util.Map;
 
-public class PaymentProcess extends Controller {
+public class PaymentController extends Controller {
+
+    static Form<Payment> paymentForm = form(Payment.class);
 
     public static Result index() {
-        Form<Payment> paymentForm              = form(Payment.class);
+
         Map<String, Appointment> appointments  = Appointment.findByUserId(Long.parseLong("1"));
+        Long total;
         return ok(views.html.checkout.render(appointments, paymentForm, Long.parseLong("2")));
+    }
+
+    public static Result processPayment() {
+        return ok();
+
     }
 }
