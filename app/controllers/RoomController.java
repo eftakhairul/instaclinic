fdcf49d29c96a287daae015b24e2e67ed0529219
Eftakhairul Islam <eftakhairul@gmail.com>
2014-03-24 20:05:27 -0400

    room listing is done

--- app/controllers/RoomController.java
+++ app/controllers/RoomController.java
@@ -6,11 +6,14 @@ import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.createSchedule;
 import views.html.editSchedule;
-import views.html.listSchedules;
-import models.Page;
+import models.Room;
+import views.html.*;
+
 
 import java.util.Date;
 import java.util.List;
+
+import static com.avaje.ebean.Ebean.validate;
 
 public class RoomController extends Controller
 {
@@ -21,17 +24,9 @@ public class RoomController extends Controller
 	    routes.ScheduleController.list(0, "name", "asc", "")
 	);
 
-    /**
-     * This serve the listing of Room
-     *
-     * @route /home
-     */
-    public static Result index() {
-       return GO_HOME;
-    }
 
   /**
-   * Display the paginated list of computers.
+   * Display the paginated list of rooms.
    *
    * @param page Current page number (starts from 0)
    * @param sortBy Column to be sorted
@@ -53,47 +48,6 @@ public class RoomController extends Controller
   }
 
   /**
-   * Display the 'edit form' of a existing Computer.
-   *
-   * @param id Id of the computer to edit
-   */
-  public static Result edit(Long id) {
-      Form<Schedule> computerForm = form(Schedule.class).fill(
-    		  Schedule.find.byId(id)
-      );
-      return ok(
-    		  editSchedule.render(id, computerForm)
-      );
-  }
-
-  /**
-   * Handle the 'edit form' submission
-   *
-   * @param id Id of the computer to edit
-   */
-  public static Result update(Long id) {
-      Form<Schedule> computerForm = form(Schedule.class).bindFromRequest();
-      if(computerForm.hasErrors()) {
-    	  //computerForm.errors()[0].
-          return badRequest(editSchedule.render(id, computerForm));
-      }
-      Schedule schedule = computerForm.get();
-      if(schedule != null) {
-    	  if(!validate(schedule)) {
-        	  return badRequest(editSchedule.render(id, computerForm));
-          }
-    	  //System.out.println("new update time: "+schedule.getId());
-    	  schedule.setId(Integer.parseInt(id.toString()));
-    	  schedule.update();
-
-    	  flash("success", "Schedule has been updated");
-    	  return GO_HOME;
-      }
-      flash("error", "Couldn't save this schedule. Please try later");
-      return badRequest(editSchedule.render(id, computerForm));
-  }
-
-  /**
    * Display the 'new computer form'.
    */
   public static Result create() {
@@ -107,26 +61,22 @@ public class RoomController extends Controller
    * Handle the 'new computer form' submission
    */
   public static Result save() {
-      Form<Schedule> scheduleForm = form(Schedule.class).bindFromRequest();
+      Form<Room> roomForm = form(Room.class).bindFromRequest();
 
-      if(scheduleForm.hasErrors()) {
+      if(roomForm.hasErrors()) {
     	  //flash("error", scheduleForm.errorsAsJson());
-          return badRequest(createSchedule.render(scheduleForm));
+          return badRequest(createRoom.render(roomForm));
       }
 
-      Schedule schedule = scheduleForm.get();
+      Room room = roomForm.get();
 
-      if(!validate(schedule)) {
-    	  return badRequest(createSchedule.render(scheduleForm));
-      }
-      //validation ends
-      
-      Long userid = Long.parseLong(session("user_id"));
-      schedule.setUser(User.findById(userid));
-      
-      schedule.save();
-      
-      flash("success", "Schedule has been created");
+//      if(!validate(room)) {
+//    	  return badRequest(createRoom.render(roomForm));
+//      }
+
+      room.save();
+      flash("success", "Room has been created");
+
       return GO_HOME;
   }
   
@@ -134,66 +84,13 @@ public class RoomController extends Controller
    * Handle computer deletion
    */
   public static Result delete(Long id) {
-	  Schedule schedule = Schedule.find.ref(id);
-	  if(Appointment.findBySchedule(schedule)) {
-		  flash("error", "This schedule can't be deleted as somone got an appointment on this schedule");
-		  return GO_HOME;
-	  }
-	  schedule.delete();
-      flash("success", "Schedule has been deleted");
+//	  Schedule schedule = Schedule.find.ref(id);
+//	  if(Appointment.findBySchedule(schedule)) {
+//		  flash("error", "This schedule can't be deleted as somone got an appointment on this schedule");
+//		  return GO_HOME;
+//	  }
+//	  schedule.delete();
+//      flash("success", "Schedule has been deleted");
       return GO_HOME;
   }
-  
-  public static boolean validate(Schedule schedule)
-  {
-	  
-	  //Validations
-      if(schedule.getEndTime().getTime() <= schedule.getStartTime().getTime()) {
-    	  flash("error", "endTime cannot be previous time then start time");
-    	  return false;
-      }
-      
-      long timeDiff = (schedule.getEndTime().getTime() - schedule.getStartTime().getTime())/60000;
-      
-      if(timeDiff == 20) {
-    	  schedule.setMeetingType(MeetingType.REGULAR);
-      }
-      else if(timeDiff == 60) {
-    	  schedule.setMeetingType(MeetingType.YEARLY);
-      }
-      else {
-    	  flash("error", "schedule must be 20 minutes or 60 minutes in length");
-          return false;
-      }
-      
-      if(schedule.getEndTime().getTime() < new Date().getTime() || schedule.getStartTime().getTime() < new Date().getTime()) {
-    	  flash("error", "Time cannot be in past time");
-    	  return false;
-      }
-      
-      if(schedule.getStartTime().getHours() < 8) {
-    	  flash("error", "Office time starts at 8:00 AM");
-    	  return false;
-      }
-      
-      if(schedule.getEndTime().getHours() > 17) {
-    	  flash("error", "Office time ends at 5:00 PM");
-    	  return false;
-      }
-      
-      User user = User.findById(Long.parseLong(session("user_id")));
-      
-      List<Schedule> schedules = Schedule.findByTime(schedule.getStartTime(), schedule.getEndTime());
-      if(schedules.size() > 0) {
-    	  for (Schedule schedule2 : schedules) {
-			if(schedule2.getUser().getId() == user.getId()) {
-				flash("error", "You have another schedule in that time frame ");
-				return false;
-			}
-    	  }
-      }
-    	  
-      return true;
-  }
-  
 }
